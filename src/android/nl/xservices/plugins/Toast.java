d24097f42f4704decd5e0cefc1c78c9aac95d8bf
Pengcheng <pengchengcao@hotmail.com>
2015-03-28 14:24:11 +1100

    update plugin

--- src/android/nl/xservices/plugins/Toast.java
+++ src/android/nl/xservices/plugins/Toast.java
@@ -2,12 +2,26 @@ package nl.xservices.plugins;
 
 import android.content.Intent;
 import android.view.Gravity;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.os.Bundle;
 
 /*
     // TODO nice way for the Toast plugin to offer a longer delay than the default short and long options
@@ -24,11 +38,26 @@ public class Toast extends CordovaPlugin {
   private static final String ACTION_SHOW_EVENT = "show";
   private static final String ACTION_LAUNCH_SEARCH = "launchSearch";
   private static final String ACTION_GET_ROUTE = "getRoute";
+  private static final String ACTION_PASS_VEHICLE = "passVehicleDetails";
+  private static final String ACTION_ADD_ACCOUNT = "addAccount";
+  private static final String ACTION_GET_AUTHTOKEN = "getAuthToken";
+
 
   private android.widget.Toast mostRecentToast;
 
+
+
+    private AccountManager mAccountManager;
+
   // note that webView.isPaused() is not Xwalk compatible, so tracking it poor-man style
   private boolean isPaused;
+
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+            mAccountManager = AccountManager.get(cordova.getActivity());
+    }
+
 
   @Override
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
@@ -82,6 +111,7 @@ public class Toast extends CordovaPlugin {
         return true;
 
     }else if(ACTION_GET_ROUTE.equals(action)){
+
         Intent intent = cordova.getActivity().getIntent();
         if(intent!=null){
             String route = intent.getStringExtra("redirRoute");
@@ -91,11 +121,128 @@ public class Toast extends CordovaPlugin {
             callbackContext.error("no routes found");
             return false;
         }
+
+    }else if(ACTION_PASS_VEHICLE.equals(action)){
+
+        Intent actionIntent = new Intent("au.gov.nswpf.PERSON_SERACH_INTENT");
+        cordova.getActivity().startActivity(actionIntent);
+        callbackContext.success();
+
+        return true;
+
+    }else if(ACTION_ADD_ACCOUNT.equals(action)){
+
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                android.widget.Toast toast = android.widget.Toast.makeText(webView.getContext(), "ADD ACCOUNT", android.widget.Toast.LENGTH_LONG);
+                toast.show();
+                mostRecentToast = toast;
+                addNewAccount("au.gov.nswpf", "Full access");
+                callbackContext.success();
+            }
+        });
+
+        //callbackContext.success();
+        return true;
+
+    }else if(ACTION_GET_AUTHTOKEN.equals(action)){
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+
+                /*
+                android.widget.Toast toast = android.widget.Toast.makeText(webView.getContext(), "GET AUTH TOKEN", android.widget.Toast.LENGTH_LONG);
+                toast.show();
+                mostRecentToast = toast;
+                */
+                //get auth token and pass back
+                Account[] accounts = getAccountsByType("au.gov.nswpf");
+                if (accounts.length>0){
+                    //String token = getExsitingAccountAuthToken(accounts[0],"Full access");
+                    final AccountManagerFuture<Bundle> future = mAccountManager.getAuthToken(accounts[0],"Full access",null,cordova.getActivity(),new AccountManagerCallback<Bundle>(){
+                        @Override
+                        public void run(AccountManagerFuture<Bundle> future) {
+                            Bundle bdl;
+                            try {
+                                bdl = future.getResult();
+                                String authToken = bdl.getString(AccountManager.KEY_AUTHTOKEN);
+                                showMessage(authToken);
+                                callbackContext.success(authToken);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                showMessage("Fetch token failed");
+                                callbackContext.error("failed");
+                            }
+                        }
+                    },null);
+
+                }else{
+                    showMessage("there's no existing account, please add a account first");
+                    //addNewAccount("au.gov.nswpf", "Full access");
+                    callbackContext.error("there's no account.");
+                }
+            }
+        });
+        return true;
+
     }else{
       callbackContext.error("toast." + action + " is not a supported function. Did you mean '" + ACTION_SHOW_EVENT + "'?");
       return false;
     }
   }
+
+
+    private void addNewAccount(String accountType, String authTokenType) {
+        final AccountManagerFuture<Bundle> future = mAccountManager.addAccount(accountType, authTokenType, null, null, cordova.getActivity(), new AccountManagerCallback<Bundle>() {
+            @Override
+            public void run(AccountManagerFuture<Bundle> future) {
+                try {
+                    Bundle bnd = future.getResult();
+                    showMessage("Account was created");
+                    Log.d("subzero", "AddNewAccount Bundle is " + bnd);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    showMessage(e.getMessage());
+                }
+            }
+        }, null);
+    }
+
+
+    //TODO:Clean this up
+    private void getExsitingAccountAuthToken(Account account, String authTokenType) {
+        final AccountManagerFuture<Bundle> future = mAccountManager.getAuthToken(account, authTokenType, null, cordova.getActivity(), new AccountManagerCallback<Bundle>() {
+            @Override
+            public void run(AccountManagerFuture<Bundle> future) {
+                Bundle bdl;
+                try {
+                    bdl = future.getResult();
+                    String authToken = bdl.getString(AccountManager.KEY_AUTHTOKEN);
+                    showMessage(authToken);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    showMessage("Fetch token failed");
+                }
+            }
+        }, null);
+    }
+
+    private Account[] getAccountsByType(String accountType){
+        Account allAcounts[] = mAccountManager.getAccountsByType(accountType);
+        return allAcounts;
+    }
+
+    private void showMessage(final String msg) {
+        if (TextUtils.isEmpty(msg))
+            return;
+
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                android.widget.Toast.makeText(webView.getContext(), msg, android.widget.Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
 
   @Override
   public void onPause(boolean multitasking) {
--- www/Toast.js
+++ www/Toast.js
@@ -39,6 +39,17 @@ Toast.prototype.launchSearch = function(person,successCallback,errorCallback){
 }
 
 
+Toast.prototype.passVehicleDetails = function(vehicle,successCallback,errorCallback){
+  cordova.exec(
+    successCallback,
+    errorCallback,
+    "Toast",
+    "passVehicleDetails",
+    [vehicle]);
+}
+
+
+
 Toast.prototype.getRoute = function(route,successCallback,errorCallback){
     cordova.exec(
         successCallback,
@@ -48,6 +59,23 @@ Toast.prototype.getRoute = function(route,successCallback,errorCallback){
         [route]);
 }
 
+Toast.prototype.getAuthToken = function(user,successCallback,errorCallback){
+	cordova.exec(
+		successCallback,
+		errorCallback,
+		"Toast",
+		"getAuthToken",
+		[user]);
+}
+
+Toast.prototype.addAccount = function(user,successCallback,errorCallback){
+	cordova.exec(
+		successCallback,
+		errorCallback,
+		"Toast",
+		"addAccount",
+		[user]);
+}
 
 Toast.install = function () {
   if (!window.plugins) {
